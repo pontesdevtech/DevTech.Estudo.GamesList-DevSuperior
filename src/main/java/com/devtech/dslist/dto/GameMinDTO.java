cc4d2280799eb1514e7f7e66c2986a211d3063e3
Tiago Pontes <pontesdevtech@outlook.com>
2025-05-25 12:25:21 -0300

    Deploy ready

--- src/main/java/com/devtech/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devtech/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devtech.dslist.dto;
 
 import com.devtech.dslist.entities.Game;
+import com.devtech.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 	private Long id;
@@ -18,6 +19,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
